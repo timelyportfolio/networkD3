2fc80fed8869319c740aec9c47926f718a608678
timelyportfolio <kent.russell@timelyportfolio.com>
2015-10-20 16:20:05 -0500

    add ncaa college football example adjacency

--- inst/examples/experiments_adjacency.R
+++ inst/examples/experiments_adjacency.R
@@ -6,6 +6,7 @@
 library(networkD3)
 library(d3heatmap)
 library(igraph)
+library(scales)
 
 data(karate, package="igraphdata")
 karate <- upgrade_graph(karate)
@@ -40,3 +41,69 @@ d3heatmap(
   cexCol = 0.5,
   height = 400, width = 400
 )
+
+
+# adjacency network with 2015 NCAA football data
+#   https://github.com/octonion/football
+
+library(dplyr)
+library(igraph)
+library(pipeR)
+
+# get game data
+"https://cdn.rawgit.com/octonion/football/master/ncaa/tsv/ncaa_games_2016.tsv" %>>%
+  read.delim(stringsAsFactors = FALSE) %>>%
+  mutate( opponent_name = gsub(x=opponent_name,pattern = "(%\\n)[\\s]{2,}", replacement = "", perl =T ) )-> games
+
+# get school meta data
+"https://cdn.rawgit.com/octonion/football/master/ncaa/tsv/ncaa_schools_2016.tsv" %>>%
+  read.delim(stringsAsFactors = FALSE) -> schools
+
+# join schools and games to only get division 1 games
+games %>>%
+  inner_join( schools ) %>>%
+  filter( division_id == 11 ) %>>%
+  inner_join( schools, c( "opponent_id" = "school_id" )) %>>%
+  filter( division_id.y == 11) %>>%
+  (
+    list(
+      nodes = rbind(
+        select(., id = school_id, name = school_name.x),
+        select(
+          .,
+          id = opponent_id,
+          name = opponent_name
+        )
+      ) %>>% unique,
+      edges = select(
+        .,
+        source = school_id,
+        target = opponent_id,
+        game_date
+      )
+    )
+  ) -> game_network
+
+graph_from_data_frame(
+  game_network$edges,
+  vertices = game_network$nodes,
+  directed = FALSE
+) %>>%
+  get.adjacency(sparse = FALSE) %>>%
+  d3heatmap(col = "Blues")
+
+# try to get colors by date
+graph_from_data_frame(
+  game_network$edges %>>%
+    mutate( game_date = as.Date(game_date,format="%m/%d/%Y")),
+  vertices = game_network$nodes,
+  directed = FALSE
+) %>>%
+  get.adjacency(sparse = FALSE, attr="game_date") %>>%
+  (
+    d3heatmap(
+      x = .,
+      cellnote = as.Date(.,origin="1970-01-01"),
+      colors = "Blues"
+    )
+  )
