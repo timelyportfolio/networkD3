d43fef208dfa1816e4262508c0e09112a45299d5
timelyportfolio <kent.russell@timelyportfolio.com>
2015-10-26 16:29:23 -0500

    add example with data.tree and flare.json

--- inst/examples/examples_flex.R
+++ inst/examples/examples_flex.R
@@ -42,3 +42,54 @@ test11 <- fromJSON(
   simplifyDataFrame = FALSE
 )
 flexNetwork(test11)
+
+
+
+
+library(data.tree)
+
+# Create URL. paste0 used purely to keep within line width.
+URL <- paste0("https://cdn.rawgit.com/christophergandrud/networkD3/",
+              "master/JSONdata//flare.json")
+
+## Convert to list format
+flare <- jsonlite::fromJSON(URL, simplifyDataFrame = FALSE)
+flare_node <- as.Node(flare, explicit = TRUE)
+flare_node$Do(
+  function(x) x$size <- Aggregate(x,"size",sum,cacheAttribute="size"),
+  traversal="post-order"
+)
+flare_df <- ToDataFrameTable(flare_node,"name","pathString","size")
+
+
+flare_tm <- treemap(
+  data.frame(
+    dplyr::rbind_all(
+      lapply(
+        strsplit(
+          gsub(
+            x = flare_df$pathString,
+            pattern = "children2/",
+            replacement = ""
+          ),
+          "/"
+        ),
+        function(x)data.frame(t(unlist(x)))
+      )
+    ),
+    flare_df
+  ),
+  index=c("X1","X2","X3"),
+  vSize="size"
+)
+
+flare_tm$tm$y_size <- flare_tm$tm$w
+flare_tm$tm$x_size <- flare_tm$tm$h
+flare_tm_d3 <- d3treeR:::convert_treemap(flare_tm$tm,rootname="flare")
+flare_tm_d3$x_size <- 1
+flare_tm_d3$y_size <- 1
+
+flexNetwork(
+  flare_tm_d3$children[[1]],
+  spacing = htmlwidgets::JS("function(a,b){return 0}")
+)
